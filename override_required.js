a7d79505d44db75184df7fbcbe69712bee7bfef4
Antevasin Software <65867585+antevasin-app@users.noreply.github.com>
2021-12-14 10:17:21 +1300

    Update override_required.js

--- override_required.js
+++ override_required.js
@@ -1,1 +1,2 @@
 $('.required').removeClass('required');
+if ( $( '#fields_537' ).length > 0 ) $( '#fields_537' ).val( 'INCOMPLETE RECORD - Used for form field testing - Required fields were overridden' );
