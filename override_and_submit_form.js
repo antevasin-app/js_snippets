f9fffcf6ba6d1afe99d97215bbf8028c52df5da9
Antevasin Software <65867585+antevasin-app@users.noreply.github.com>
2021-12-15 12:59:39 +1300

    Update override_and_submit_form.js

--- override_and_submit_form.js
+++ override_and_submit_form.js
@@ -1,3 +1,3 @@
 $('.required').removeClass('required');
 if ( $( '#fields_537' ).length > 0 ) $( '#fields_537' ).val( 'INCOMPLETE RECORD - Used for form field testing - Required fields were overridden' );
-$( 'form' ).submit();
+$( '.btn-primary-modal-action' ).trigger( 'click' )
